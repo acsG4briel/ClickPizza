cfd5ad29d756889159949b3934bd4499833ccc0f
Gabriel Silva <gabriel.araujocsilv@gmail.com>
2025-04-29 09:55:50 -0300

    PARTIAL - Configuracoes deploy 4

--- site/src/services/formaPagamento.js
+++ site/src/services/formaPagamento.js
@@ -1,14 +1,29 @@
 import axios from 'axios';
 
-const URL_BASE =  'https://localhost:44329/Pagamento';
+// const URL_BASE =  'https://localhost:44329/Pagamento';
+
+// //ENDPOINT OBTER FORMAS PAGAMENTO POR USUARIO ID
+// export const getFormasPagamento = async (usuarioId) => {
+//     try {
+//       const response = await axios.get(`${URL_BASE}/${usuarioId}`);
+//       return response.data;
+//     } catch (error) {
+//       console.error('Erro ao buscar formas de pagamento:', error);
+//       throw error;
+//     }
+//     };
+
+//PRODUCAO
+
+const URL_BASE =  'https://clickpizza-production.up.railway.app/Pagamento';
 
 //ENDPOINT OBTER FORMAS PAGAMENTO POR USUARIO ID
 export const getFormasPagamento = async (usuarioId) => {
-    try {
-      const response = await axios.get(`${URL_BASE}/${usuarioId}`);
-      return response.data;
-    } catch (error) {
-      console.error('Erro ao buscar formas de pagamento:', error);
-      throw error;
-    }
-    };
+try {
+  const response = await axios.get(`${URL_BASE}/${usuarioId}`);
+  return response.data;
+} catch (error) {
+  console.error('Erro ao buscar formas de pagamento:', error);
+  throw error;
+}
+};
--- site/src/services/users.js
+++ site/src/services/users.js
@@ -1,9 +1,24 @@
 import axios from "axios";
 
-//ENDPOINT OBTER USUARIO  
+// //ENDPOINT OBTER USUARIO  
+// export const getUsuario = async (usuarioId) => {
+//   try {
+//     const response = await axios.get('https://localhost:44329/api/Usuario', {
+//       params: { usuarioId }
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.error('Erro ao buscar usuário:', error);
+//     throw error;
+//   }
+//  };
+
+//PRODUCAO
+
+// ENDPOINT OBTER USUARIO  
 export const getUsuario = async (usuarioId) => {
   try {
-    const response = await axios.get('https://localhost:44329/api/Usuario', {
+    const response = await axios.get('https://clickpizza-production.up.railway.app/api/Usuario', {
       params: { usuarioId }
     });
     return response.data;
